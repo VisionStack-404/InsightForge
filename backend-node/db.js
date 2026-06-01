c9c2d09ff1398edcd3eb64aa1ae5199c717d7260
Varun <189742027+VisionStack-404@users.noreply.github.com>
2026-06-01 12:12:47 +0530

    Update MongoDB connection log message

--- backend-node/db.js
+++ backend-node/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log("MongoDB is connected"))
   .catch(err => console.error(err));
