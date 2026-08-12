aee1b0cc48a1a6e2ad93611634e96db81e624629
Varun <189742027+VisionStack-404@users.noreply.github.com>
2026-08-12 22:10:47 +0530

    Add comment for mongo_uri variable

--- worker-python/db.py
+++ worker-python/db.py
@@ -1,6 +1,6 @@
 import os
 from pymongo import MongoClient
-
+#adding of the mongo_url
 mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 client = MongoClient(mongo_uri)
 db = client["readinglist"]
